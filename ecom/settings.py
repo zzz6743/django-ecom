a8a63c6c86b07becc8b435e16f8b6bde53bf89bd
HABiBi <zone6743@gmail.com>
2024-07-10 20:10:41 +0700

    added DB Password environ

--- ecom/settings.py
+++ ecom/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
         'HOST': 'monorail.proxy.rlwy.net',
         'PORT': '32001',
     }
